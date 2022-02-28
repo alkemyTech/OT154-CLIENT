31ccbb40b8bf70ee981fdfc57b3f022e85005363
leandriel <leandro88valderas@gmail.com>
2022-02-28 12:43:43 -0300

    all conflict fixeds

--- app/src/main/java/com/melvin/ongandroid/model/apiservice/RetrofitInstance.kt
+++ app/src/main/java/com/melvin/ongandroid/model/apiservice/RetrofitInstance.kt
@@ -1,13 +1,6 @@
 package com.melvin.ongandroid.model.apiservice
 
-<<<<<<< HEAD
 import com.melvin.ongandroid.data.apiservice.ApiConstants
-=======
-import com.melvin.ongandroid.constantsapi.ApiConstants
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-
->>>>>>> 242e7239aabdb1dd26c2e781d3c43ec6aa0de220
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
