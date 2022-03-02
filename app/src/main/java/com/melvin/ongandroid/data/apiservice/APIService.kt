3d7179369bf6b69efab3a3d2d5f18be05089c33f
Mauro Peña <mauroalejandropena@gmail.com>
2022-03-01 23:56:25 -0300

    added api registration logic.

--- app/src/main/java/com/melvin/ongandroid/data/apiservice/APIService.kt
+++ app/src/main/java/com/melvin/ongandroid/data/apiservice/APIService.kt
@@ -3,17 +3,24 @@ package com.melvin.ongandroid.data.apiservice
 import com.melvin.ongandroid.data.response.ResponseApi
 import com.melvin.ongandroid.model.Contact
 import com.melvin.ongandroid.model.Testimonial
+import com.melvin.ongandroid.model.apiservice.apimodel.NewUserResponse
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface APIService {
     //Tomamos informacion desde testimonios en la API - Leandro Valderas
-    @GET( "api/testimonials")
+    @GET("api/testimonials")
     suspend fun getTestimonials(): ResponseApi<MutableList<Testimonial>>
 
     @POST("api/contacts")
     suspend fun postContacts(@Body contactData: Contact): Call<Contact>
 
+    @FormUrlEncoded
+    @POST("api/register")
+
+    fun postNewUser(
+        @Field("email") email: String,
+        @Field("name") name: String,
+        @Field("password") password: String
+    ): Call<NewUserResponse>
 }
